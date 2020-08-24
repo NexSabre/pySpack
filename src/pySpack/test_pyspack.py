0abf0cb854b0a48ec552a607cd97ccc2d7bc7e95
Nex Sabre <NexSabre@users.noreply.github.com>
2020-08-24 20:40:10 +0200

    Add tests for load and github actions

--- src/pySpack/test_pyspack.py
+++ src/pySpack/test_pyspack.py
@@ -1,4 +1,5 @@
 import unittest
+from build.lib.pySpack import pyspack
 
 from pySpack.pyspack import PySpack
 
@@ -24,3 +25,23 @@ class Test_TestPySpack(unittest.TestCase):
     def test_uninstall(self):
         self.pyspack.install('py-json5')
         self.assertTrue(self.pyspack.uninstall('py-json5'), "py-json5 should be uninstalled correctly")
+
+    def test_load(self):
+        self.pyspack.install('py-json5')
+        load_output = self.pyspack.load('py-json5')
+        self.assertTrue(self.find_python_path(load_output, 'py-json5'), "py-json5 should be available at PYTHONPATH string returned by the Spack")
+
+    def test_negative_load(self):
+        package_which_not_exists = 'py-random-package-which-does-not-exists'
+        load_output = self.pyspack.load(package_which_not_exists)
+        self.assertFalse(self.find_python_path(load_output, package_which_not_exists))
+
+    @staticmethod
+    def find_python_path(decoded_output: str, package_to_find: str) -> bool:
+        exports_list = [x for x in str(decoded_output).split(';') if 'PYTHONPATH' in x]
+        for element in exports_list: 
+            if package_to_find in element:
+                break
+        else:
+            return False        
+        return True
